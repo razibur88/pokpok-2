bb62ea6a98f2426dac8d120a89bd80551997e75c
Shohelrana63 <shohelrana.beg@gmail.com>
2024-10-22 16:02:12 +0600

    Paginataion add

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,31 +1,13 @@
-import React, { useContext, useState } from "react";
+import React, {  useState } from "react";
 import Container from "../components/Container";
-import { FaPlus, FaMinus } from "react-icons/fa6";
-import Post from "../components/Post";
-import { ApiData } from "../components/ContextApi";
+import { FaMinus, FaPlus } from "react-icons/fa";
 import Pagination from "../components/Pagination";
 
+
+
 const Shop = () => {
-  let {info, loading} = useContext(ApiData)
+  
   let [show, setShow] = useState(false);
-  let [currentPage, setCurrentPage] = useState(1)
-  let [perPage, setPerPage] = useState(6)
-
-  let lastPage = currentPage * perPage
-  let firstPage = lastPage - perPage
-  let allPage = info.slice(firstPage, lastPage)
-
-  let pageNumber = []
-
-  for(let i = 0; i < Math.ceil(info.length / perPage); i++){
-    pageNumber.push(i)
-  }
-
-  console.log(pageNumber);
-  
-  
-  
-
 
   return (
     <section>
@@ -55,45 +37,8 @@ const Shop = () => {
             </div>
           </div>
           <div className="w-4/5">
-            <div className="pt-8 flex justify-end items-center gap-x-10">
-              <div className="">
-                <label className="pr-3" htmlFor="">
-                  Sort By :
-                </label>
-                <select
-                  className="w-[60px] h-[30px] border-[1px] border-[#262626]"
-                  name=""
-                  id=""
-                >
-                  <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
-                </select>
-              </div>
-              <div className="">
-                <label className="pr-3" htmlFor="">
-                  Show:
-                </label>
-                <select
-                  className="w-[60px] h-[30px] border-[1px] border-[#262626]"
-                  name=""
-                  id=""
-                >
-                  <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
-                  <option value="">one</option>
-                </select>
-              </div>
-            </div>
-
-            <div className="flex justify-between flex-wrap">
-              <Post allPage={allPage} />
-              <div className="py-10 flex justify-center w-full">
-              <Pagination pageNumber={pageNumber}/>
-              </div>
-            </div>
+          <Pagination itemsPerPage={9} />
+        
           </div>
         </div>
       </Container>
